283ea672ad8103f2eefe43989a96282746bb646b
galtashma <galtashma@gmail.com>
2019-10-15 12:18:33 +0300

    First commit, basic emulation

---
+++ install.sh
@@ -0,0 +1,3 @@
+#brew install unicorn
+#DYLD_LIBRARY_PATH=/usr/local/Cellar/unicorn/1.0.1 pip install unicorn --user
+DYLD_LIBRARY_PATH=/usr/local/Cellar/keystone/0.9.1 pip install keystone-engine --user
---
+++ main.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+import frontmatter
+
+from unicorn import *
+from unicorn.arm64_const import *
+from keystone import *
+
+def get_register_for_key(key):
+    return KEY_TO_REG[key]
+
+KEY_TO_REG = {
+    "X0": UC_ARM64_REG_X0,
+    "X1": UC_ARM64_REG_X1,
+    "X2": UC_ARM64_REG_X2,
+    "X3": UC_ARM64_REG_X3,
+    "X4": UC_ARM64_REG_X4,
+    "X5": UC_ARM64_REG_X5,
+    "X6": UC_ARM64_REG_X6,
+    "X7": UC_ARM64_REG_X7,
+    "X8": UC_ARM64_REG_X8,
+    "X9": UC_ARM64_REG_X9,
+    "X10": UC_ARM64_REG_X10,
+    "X11": UC_ARM64_REG_X11,
+    "X12": UC_ARM64_REG_X12,
+    "X13": UC_ARM64_REG_X13,
+    "X14": UC_ARM64_REG_X14,
+    "X15": UC_ARM64_REG_X15,
+    "X16": UC_ARM64_REG_X16,
+    "X17": UC_ARM64_REG_X17,
+    "X18": UC_ARM64_REG_X18,
+    "X19": UC_ARM64_REG_X19,
+    "X20": UC_ARM64_REG_X20,
+    "X21": UC_ARM64_REG_X21,
+    "X22": UC_ARM64_REG_X22,
+    "X23": UC_ARM64_REG_X23,
+    "X24": UC_ARM64_REG_X24,
+    "X25": UC_ARM64_REG_X25,
+    "X26": UC_ARM64_REG_X26,
+    "X27": UC_ARM64_REG_X27,
+    "X28": UC_ARM64_REG_X28,
+    #"SP": UC_ARCH_ARM64_SP,
+    #"PC": UC_ARCH_ARM64_PC,
+}
+
+def hook_block(uc, address, size, user_data):
+    print(">>> Tracing basic block at 0x%x, block size = 0x%x" %(address, size))
+
+def hook_code(uc, address, size, user_data):
+    print(">>> Tracing instruction at 0x%x, instruction size = 0x%x" %(address, size))
+
+
+
+# memory address where emulation starts
+ADDRESS    = 0x10000
+
+class Emulator(object):
+    def __init__(self):
+        self.mu = Uc(UC_ARCH_ARM64, UC_MODE_ARM)
+        self.ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN) 
+        
+        self.registers = {} 
+        self.asm = "";
+
+    def load_instructions(self, path):
+        data = frontmatter.load(path)
+        for k,v in data.to_dict().items():
+            if k == "content":
+                self._set_asm(v)
+            else:
+                self.registers[k] = v
+
+    def _set_asm(self, asm):
+        self.asm = asm
+        encoding, count = self.ks.asm(asm)
+        self.code = b''.join([chr(x) for x in encoding])
+        self.code_len = len(self.code)
+
+    def start(self):
+        self.mu.mem_map(ADDRESS, 2 * 1024 * 1024)
+        self.mu.mem_write(ADDRESS, self.code)
+        [self.mu.reg_write(get_register_for_key(reg), val) for reg,val in self.registers.items()]
+
+        self.mu.reg_write(UC_ARM64_REG_X11, 0x12345678)
+        self.mu.reg_write(UC_ARM64_REG_X13, 0x10008)
+        self.mu.reg_write(UC_ARM64_REG_X15, 0x33)
+
+        self.mu.hook_add(UC_HOOK_BLOCK, hook_block)
+        self.mu.hook_add(UC_HOOK_CODE, hook_code, begin=ADDRESS, end=ADDRESS)
+        print ("0x%x - 0x%x" % (ADDRESS, ADDRESS + self.code_len))
+        self.mu.emu_start(ADDRESS, ADDRESS + self.code_len)
+        print (">>> Emulation done.")
+        print (">>> X0: 0x%x" % self.mu.reg_read(UC_ARM64_REG_X0))
+        print (">>> X1: 0x%x" % self.mu.reg_read(UC_ARM64_REG_X1))
+
+
+def main():
+    e = Emulator()
+    e.load_instructions('test.inst')
+    print ("asm", e.asm)
+    print ("registers", e.registers)
+    e.start()
+
+if __name__ == '__main__':
+    main()
