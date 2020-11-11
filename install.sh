72b286240466180c420b0567f25b193977c62862
galtashma <gal.tashma@swg.local>
2020-11-11 18:40:28 +0200

    clean up installs

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+pip3 install -r requirements.txt
 #brew install unicorn
 #DYLD_LIBRARY_PATH=/usr/local/Cellar/unicorn/1.0.1 pip install unicorn --user
-DYLD_LIBRARY_PATH=/usr/local/Cellar/keystone/0.9.1 pip install keystone-engine --user
+#DYLD_LIBRARY_PATH=/usr/local/Cellar/keystone/0.9.1 pip install keystone-engine --user
